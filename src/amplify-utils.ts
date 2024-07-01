e90af4323855110e51896da8cf3daf1b1415febf
offlineprogrammer <mmalaka@gmail.com>
2024-06-30 20:27:26 -0700

    connect to bedrock

--- src/amplify-utils.ts
+++ src/amplify-utils.ts
@@ -2,13 +2,13 @@ import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 
 import { generateClient } from "aws-amplify/data";
+import { type Schema } from "../amplify/data/resource"
 import outputs from "../amplify_outputs.json";
-/**
- * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
- */
+
+
 
 Amplify.configure(outputs);
 
-export const amplifyClient = generateClient({
+export const amplifyClient = generateClient<Schema>({
   authMode: "userPool",
 });
