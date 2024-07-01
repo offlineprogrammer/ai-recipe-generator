cd3ea456eec0f0a5e5517bf7e91329299a4494ad
offlineprogrammer <mmalaka@gmail.com>
2024-07-01 07:59:14 -0700

    connect to bedrock

--- src/amplify-utils.ts
+++ src/amplify-utils.ts
@@ -1,28 +1,17 @@
-// import config from "../amplify_outputs.json";
-// import { Amplify } from "aws-amplify";
-// import { generateClient } from "aws-amplify/data";
-// import { type Schema } from "../amplify/data/resource";
-// /**
-// * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
-// */
-// Amplify.configure(config);
 
-// export const amplifyClient: import('aws-amplify/data').Client<import('../amplify/data/resource').Schema> = generateClient<Schema>();
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
 
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+import { type Schema } from "../amplify/data/resource";
 
-// // import { Amplify } from "aws-amplify";
-// // import "@aws-amplify/ui-react/styles.css";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
 
-// // import { generateClient } from "aws-amplify/data";
-// // import outputs from "../amplify_outputs.json";
-// // import { type Schema } from "../amplify/data/resource";
+Amplify.configure(outputs);
 
-// // /**
-// //  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
-// //  */
-
-// // Amplify.configure(outputs);
-
-// // export const amplifyClient = generateClient<Schema>({
-// //   authMode: "userPool",
-// // });
+export const amplifyClient = generateClient<Schema>({
+  authMode: "userPool",
+});
